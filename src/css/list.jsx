a0942ba64b47f4d24c8e3a88bf3bd03813aff97d
ssek69420 <kaikevix2233@gmail.com>
2024-10-28 20:08:20 -0300

    started things
    doesn't work properly (valve pls fix)

--- src/css/list.jsx
+++ src/css/list.jsx
@@ -1,30 +1,29 @@
-import styles from './List.module.css'
+import styles from "./List.module.css";
 
 export default function List({ lista }) {
-    return (
-        <>
-<ul>
-            <div className={styles.home}>
-                <h1>Lista de Produtos</h1>
-                {lista.map((produto) =>
-                    <li key={produto.id}>
-                        <div className="card-container">
-                            <div className="card">
-                            <div className="title">
-                            <h2>{produto.title}</h2>
-                        </div>
-                        <p>{produto.description}</p>
-                        <p>Preço: R${produto.price}</p>
-                        <div className="image-thing">
-                            <img src={produto.image} alt={produto.title} width={100} />
-                        </div>
-                            </div>
-                        </div>
-                        
-                    </li>
-                )}
-            </div>
-</ul>
-        </>
-    )
-} 
+  return (
+    <>
+      <ul>
+    <div className={styles.home}>
+        <h1>Lista de Produtos</h1>
+    {lista.map((produto) => (
+            <li key={produto.id}>
+              <div className="card-container">
+                <div className="card">
+                  <div className="title">
+                    <h2>{produto.title}</h2>
+                  </div>
+                  <p>{produto.description}</p>
+                  <p>Preço: R${produto.price}</p>
+                  <div className="image-thing">
+                    <img src={produto.image} alt={produto.title} width={100} />
+                  </div>
+                </div>
+              </div>
+            </li>
+    ))}
+        </div>
+      </ul>
+    </>
+  );
+}
