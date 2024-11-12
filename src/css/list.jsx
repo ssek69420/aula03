df7932a8a868ca90163e4740bf10663432d193d3
ssek69420 <kaikevix2233@gmail.com>
2024-11-11 22:42:54 -0300

    finished?
    it still kinda looks like shit but hey it works

--- src/css/list.jsx
+++ src/css/list.jsx
@@ -2,36 +2,22 @@ import styles from "./List.module.css";
 
 export default function List({ lista }) {
   return (
-    <>
-      <ul>
-    <div className={styles.home}>
-        <h1 className={styles.listproductstitle}>Lista de Produtos</h1>
-    {lista.map((produto) => (
-            <li key={produto.id}>
-              <div className={styles.cardcontainer}>
-
-                <div className={styles.card}>
-
-                  <div className={styles.imagething}>
-
-                    <img src={produto.image} alt={produto.title} width={100} />
-                  
-                  </div>
-
-                  <div className={styles.title}>
-                    <h2>{produto.title}</h2>
-                  </div>
-
-                  <p>{produto.description}</p>
-
-                  <p>Preço: R${produto.price}</p>
-
-                </div>
-              </div>
-            </li>
-    ))}
-        </div>
+    <div className="globalThing">
+      <h1 className={styles.listproductstitle}>Lista de Produtos</h1>
+      <ul className={styles.cardcontainer}>
+        {lista.map((produto) => (
+          <li key={produto.id} className={styles.card}>
+            <div className={styles.imagething}>
+              <img src={produto.image} alt={produto.title} width={100} />
+            </div>
+            <div className={styles.title}>
+              <h2>{produto.title}</h2>
+            </div>
+            <p>{produto.description}</p>
+            <p>Preço: R${produto.price}</p>
+          </li>
+        ))}
       </ul>
-    </>
+    </div>
   );
 }
